455ad4680650e4cb46c5460930332d32e5407cdb
vira2121 <virajadhav2121@gmail.com>
2022-03-24 16:35:20 +0530

    updated

--- UnittestingExamples.py
+++ UnittestingExamples.py
@@ -8,6 +8,24 @@ def check(x):
         return "odd"
 
 
+def CheckDivisibleby7(x):
+    if x%7 == 0:
+        return True
+    else:
+        return False
+
+class CheckDivisible(unittest.TestCase):
+    def test_case_divisible(self):
+        x = 14
+        result = CheckDivisibleby7(x)
+        self.assertTrue(result)
+
+    def test_case_divisible1(self):
+        x = 9
+        result = CheckDivisibleby7(x)
+        self.assertFalse(result)
+
+
 class EvenOrOddApp(unittest.TestCase):
     def test_case_even_check(self):
         x = 10
